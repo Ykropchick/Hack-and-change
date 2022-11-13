70e1cda624fd2166c702e77b634686ac6c676bb6
Kirill <shkolenko02-03-2004@mail.ru>
2022-11-13 11:11:14 +0300

    The finish project

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .models import *
-from django.core.exceptions import ValidationError
 from django.contrib.auth.views import LoginView, AuthenticationForm
 
 
--- main/views.py
+++ main/views.py
@@ -1,12 +1,6 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
-from django import forms
-from django.contrib.auth.forms import User
-from .models import Streamers
 from .forms import SignUp, SignIn
 
 
@@ -26,38 +20,16 @@ def donate_page(request):
     return render(request, 'main/donate_page.html')
 
 
-# def register(request):
-#     if request.method == "Post":
-#         form = SignUp(request.POST)
-#         new_user = form.save(commit=False)
-#
 def register(request):
     if request.method == 'POST':
         f = SignUp(request.POST)
         if f.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = f.save(commit=False)
-            # Set the chosen password
-            # Save the User object
             new_user.save()
         return render(request, 'main/main_page.html', {'new_user': new_user})
     else:
         f = SignUp()
     return render(request, 'main/register_page.html', {'form': f})
-
-
-# def login(request):
-#     if request.method == 'POST':
-#         f = SignIn(request.POST)
-#         if f.is_valid():
-#             phone = f.cleaned_data.get("phone")
-#             password = f.cleaned_data.get("password")
-#             user = Streamers.objects.all()
-#             if user and user['password'] == password:
-#                 return render(request, 'main/main_page.html')
-#     else:
-#         f = SignUp()
-#     return render(request, 'main/enter_page.html', {'form': f})
 
 
 class LoginUser(LoginView):
